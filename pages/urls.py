6259a21370cca792399a2ba8ad8cf968c8c764a7
Clay <claywightman@gmail.com>
2022-04-29 22:01:05 -0400

    updated resume page

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import HomePageView, ResumePageView, AboutMeView
-
+from .views import HomePageView, AboutMeView, SiteInfoView, ResumeView
 urlpatterns = [
     path('', HomePageView.as_view(), name = 'home'),
-    path('resume/', ResumePageView.as_view(), name = "resume"),
-    path('about_me/', AboutMeView.as_view(), name = "about_me")
+    path('about_me/', AboutMeView.as_view(), name = "about_me"),
+    path('site_info/', SiteInfoView.as_view(), name = "site_info"),
+    path('resume/', ResumeView.as_view(), name = "resume"),
+    path('download_resume/<str:filename>/', ResumeView.download_resume, name = "resume_download"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+import mimetypes
+import os
+from django.http.response import HttpResponse
 
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
-class ResumePageView(TemplateView):
-    template_name = 'pages/resume.html'
 
 class AboutMeView(TemplateView):
     template_name = 'pages/about_me.html'
+
+class SiteInfoView(TemplateView):
+    template_name = 'pages/site_info.html'
+
+class ResumeView(TemplateView):
+    template_name = 'pages/resume.html'
+    def download_resume(request, filename = ''):
+        # Define Django project base directory
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        # Define the full file path
+        filepath = BASE_DIR + '/static/files/' + filename
+        # Open the file for reading content
+        path = open(filepath, 'rb')
+        # Set the mime type
+        mime_type, _ = mimetypes.guess_type(filepath)
+        # Set the return value of the HttpResponse
+        response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+        response['Content-Disposition'] = "attachment; filename=%s" % filename
+        # Return the response value
+        return response
